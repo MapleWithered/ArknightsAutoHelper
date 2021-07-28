814d677fefe8d01e40d3527c914a624024935499
MapleWithered <47618005+MapleWithered@users.noreply.github.com>
2021-07-28 23:58:03 +0800

    修改bat默认分页为utf8，并加入可选终端

--- 无人值守(Windows Terminal).bat
+++ 无人值守(Windows Terminal).bat
@@ -1,5 +1,13 @@
 @echo off
 
+if not defined TAG (
+    set TAG=1
+    start wt -p "Windows PowerShell" %0
+    :: Windows Terminal 中 cmd 的配置名，我这里是“cmd”
+    exit
+)
+
+
 rem 切换至 ArknightsAutoHelper 所在位置
 :path
 cd>nul 2>nul /D %~dp0
