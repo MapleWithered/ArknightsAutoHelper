950c98cfe1fd9136c0e92c7c767579736ec5eaa5
MapleWithered <47618005+MapleWithered@users.noreply.github.com>
2021-08-15 11:16:37 +0800

    Reformat file

--- run_plan_996.py
+++ run_plan_996.py
@@ -11,10 +11,10 @@ import schedule
 
 list_not_open = []
 
-def run_plan():
 
+def run_plan():
     helper, _ = _create_helper()
-    
+
     assert os.path.exists('user_file/plan.json'), '未能检测到刷图计划文件.'
 
     with open('user_file/plan.json', 'r', encoding='utf-8') as f:
@@ -37,15 +37,17 @@ def run_plan():
             for i in range(len(stages_same_prior)):
                 stage = stages_same_prior[i]
                 remain_ratio = stage.get('remain', stage['count']) / stage['count']
-                if stage['stage'] not in list_not_open and stage.get('remain', stage['count']) > 0 and remain_ratio > max_remain_ratio:
+                if stage['stage'] not in list_not_open and stage.get('remain', stage[
+                    'count']) > 0 and remain_ratio > max_remain_ratio:
                     stage_ok_id = i
                     max_remain_ratio = remain_ratio
             if stage_ok_id == -1:
                 logger.warning('优先级 ' + str(priority_id) + ' 无剩余未完成开放关卡')
-                continue # 没有未完成的开放关卡，下一优先级
+                continue  # 没有未完成的开放关卡，下一优先级
             stage = stages_same_prior[stage_ok_id]
             remain = stage.get('remain', stage['count'])
-            logger.warning('优先级: %s, 关卡 [%s], 总计划: %s, 剩余次数: %s, 备注: %s' % (priority_id, stage['stage'], stage['count'], remain, stage['//']))
+            logger.warning('优先级: %s, 关卡 [%s], 总计划: %s, 剩余次数: %s, 备注: %s' % (
+            priority_id, stage['stage'], stage['count'], remain, stage['//']))
             try:
                 # 执行未完成的开放关卡一次
                 c_id, remain = helper.module_battle(stage['stage'], 1)
@@ -54,7 +56,7 @@ def run_plan():
                 logger.info('关卡 [%s] 未开放, 继续下一关卡' % stage['stage'])
                 list_not_open.append(stage['stage'])
                 logger.info('当日未开放关卡列表：' + str(list_not_open))
-                break # 重新进行优先级遍历
+                break  # 重新进行优先级遍历
             except ValueError:
                 # 非主线芯片关卡（即，故事集/活动/剿灭）
                 helper.back_to_main()
@@ -73,7 +75,7 @@ def run_plan():
                         # 用户不希望录制 当日内忽略此关卡
                         list_not_open.append(stage['stage'])
                         logger.info(f"今日将忽略 {stage['stage']} 关卡")
-                        break # 重新进行优先级遍历
+                        break  # 重新进行优先级遍历
                     # 希望录制 判断支线关卡/剿灭作战
                     if stage['stage'].find('-') != -1:
                         # 支线活动关卡
@@ -85,8 +87,8 @@ def run_plan():
                         print(f'请在点击后等待 {wait_seconds_after_touch} s , 待控制台出现 "继续..." 字样, 再进行下一次点击.')
                         print(f'准备开始录制 {record_name}...')
                         helper.create_custom_record(record_name, roi_size=64,
-                                                        description=f"从主线一路点击进入{stage_category}关卡（章节）",
-                                                        wait_seconds_after_touch=wait_seconds_after_touch)
+                                                    description=f"从主线一路点击进入{stage_category}关卡（章节）",
+                                                    wait_seconds_after_touch=wait_seconds_after_touch)
                     else:
                         # 剿灭作战
                         wait_seconds_after_touch = 2
@@ -97,14 +99,15 @@ def run_plan():
                         print(f'请在点击后等待 {wait_seconds_after_touch} s , 待控制台出现 "继续..." 字样, 再进行下一次点击.')
                         print(f'准备开始录制 {record_name}...')
                         helper.create_custom_record(record_name, roi_size=64,
-                                                        description=f"从主线一路点击进入{stage_category}关卡",
-                                                        wait_seconds_after_touch=wait_seconds_after_touch)
+                                                    description=f"从主线一路点击进入{stage_category}关卡",
+                                                    wait_seconds_after_touch=wait_seconds_after_touch)
                     # 录完了 刷一波
                     if stage['stage'].find('-') != -1:
                         _, stage_map_linear = get_stage(stage['stage'])
                         while True:
                             try:
-                                helper.find_and_tap_stage_by_ocr(partition=None, target=stage['stage'], partition_map=stage_map_linear)
+                                helper.find_and_tap_stage_by_ocr(partition=None, target=stage['stage'],
+                                                                 partition_map=stage_map_linear)
                             except RuntimeError:
                                 # 活动关识别失败，程序重新尝试识别
                                 logger.info('关卡 [%s] 识别失败, 尝试重新识别' % stage['stage'])
@@ -125,10 +128,11 @@ def run_plan():
                             logger.info('关卡 [%s] 未开放, 继续下一关卡' % stage['stage'])
                             list_not_open.append(stage['stage'])
                             logger.info('当日未开放关卡列表：' + str(list_not_open))
-                            break # 重新进行优先级遍历
+                            break  # 重新进行优先级遍历
                         while True:
                             try:
-                                helper.find_and_tap_stage_by_ocr(partition=None, target=stage['stage'], partition_map=stage_map_linear)
+                                helper.find_and_tap_stage_by_ocr(partition=None, target=stage['stage'],
+                                                                 partition_map=stage_map_linear)
                             except RuntimeError:
                                 # 活动关识别失败，程序重新尝试识别
                                 logger.info('关卡 [%s] 识别失败, 尝试重新识别' % stage['stage'])
@@ -138,51 +142,51 @@ def run_plan():
                     else:
                         c_id, remain = helper.module_battle_slim(None, 1)
 
-            if remain == 1: # 理智不足未进行单次任务执行
+            if remain == 1:  # 理智不足未进行单次任务执行
                 has_remain_sanity = False
                 # 退出遍历
                 break
-            else: # 成功执行一次任务
+            else:  # 成功执行一次任务
                 stage['remain'] = stage.get('remain', stage['count']) - 1
                 with open('user_file/plan.json', 'w', encoding='utf-8') as f:
-                    json.dump(plan, f, indent=4, sort_keys=False, ensure_ascii = False)
-                break # 重新进行优先级遍历
+                    json.dump(plan, f, indent=4, sort_keys=False, ensure_ascii=False)
+                break  # 重新进行优先级遍历
 
     helper.back_to_main()
     logger.info('理智已清空')
 
 
 def run_friend():
-
     helper, _ = _create_helper()
 
     logger.warning('开始访问好友')
     helper.get_credit()
     helper.back_to_main()
 
-def run_ship():
 
+def run_ship():
     helper, _ = _create_helper()
 
     logger.warning('开始收基建')
     helper.get_building()
     helper.back_to_main()
 
-def run_task():
 
+def run_task():
     helper, _ = _create_helper()
 
     logger.warning('开始收任务奖励')
     helper.clear_task()
     helper.back_to_main()
 
-def print_plan():
 
+def print_plan():
     with open('user_file/plan.json', 'r', encoding='utf-8') as f:
         plan = json.load(f)
-    
+
     print_plan_with_plan(plan)
     print_sanity_usage(plan)
+
 
 def get_good_stage_id(stages_same_prior):
     stage_ok_id = -1
@@ -190,7 +194,8 @@ def get_good_stage_id(stages_same_prior):
     for i in range(len(stages_same_prior)):
         stage = stages_same_prior[i]
         remain_ratio = stage.get('remain', stage['count']) / stage['count']
-        if stage['stage'] not in list_not_open and stage.get('remain', stage['count']) > 0 and remain_ratio > max_remain_ratio:
+        if stage['stage'] not in list_not_open and stage.get('remain',
+                                                             stage['count']) > 0 and remain_ratio > max_remain_ratio:
             stage_ok_id = i
             max_remain_ratio = remain_ratio
     return stage_ok_id
@@ -219,11 +224,18 @@ def print_plan_with_plan(plan):
             else:
                 status_char = ' '
             if fini_percent == 100 or task['count'] == 9999 or fini_percent == 0:
-                logger.info(str(prior).ljust(6) + task['stage'].ljust(14-int((len(task['stage'].encode('utf-8'))-len(task['stage']))/2)) + str(task.get('sanity', '')).ljust(7) + str(task['count']).ljust(7) + str(remain).ljust(7) + " --  " + status_char + "    " + task['//'])
+                logger.info(str(prior).ljust(6) + task['stage'].ljust(
+                    14 - int((len(task['stage'].encode('utf-8')) - len(task['stage'])) / 2)) + str(
+                    task.get('sanity', '')).ljust(7) + str(task['count']).ljust(7) + str(remain).ljust(
+                    7) + " --  " + status_char + "    " + task['//'])
             else:
-                logger.info(str(prior).ljust(6) + task['stage'].ljust(14-int((len(task['stage'].encode('utf-8'))-len(task['stage']))/2)) + str(task.get('sanity', '')).ljust(7) + str(task['count']).ljust(7) + str(remain).ljust(7) + str(fini_percent).rjust(3) + "% " + status_char + "    " + task['//'])
+                logger.info(str(prior).ljust(6) + task['stage'].ljust(
+                    14 - int((len(task['stage'].encode('utf-8')) - len(task['stage'])) / 2)) + str(
+                    task.get('sanity', '')).ljust(7) + str(task['count']).ljust(7) + str(remain).ljust(7) + str(
+                    fini_percent).rjust(3) + "% " + status_char + "    " + task['//'])
         logger.info("---------------------------------------------------------------------------------------")
         prior += 1
+
 
 def print_sanity_usage(plan):
     prior = 1
@@ -250,24 +262,23 @@ def print_sanity_usage(plan):
             break
     if now_prior != -1:
         sanity_usage = 0
-        for task in plan['stages'][now_prior-1]['stages']:
+        for task in plan['stages'][now_prior - 1]['stages']:
             sanity_usage += task.get('remain', task['count']) * task.get('sanity', 0)
         hour_rest = sanity_usage / 240 * 24
         hour_rest_monthly = sanity_usage // 300 * 24 + sanity_usage % 300 / 10
         if hour_rest >= 24:
-            str_hour_rest = str(int(hour_rest//24)) + " 天 " + str(int(hour_rest%24)+1) + " 小时"
+            str_hour_rest = str(int(hour_rest // 24)) + " 天 " + str(int(hour_rest % 24) + 1) + " 小时"
         else:
-            str_hour_rest = str(int(hour_rest%24)+1) + " 小时"
+            str_hour_rest = str(int(hour_rest % 24) + 1) + " 小时"
         if hour_rest_monthly >= 24:
-            str_hour_rest_monthly = str(int(hour_rest_monthly//24)) + " 天 " + str(int(hour_rest_monthly%24)+1) + " 小时"
+            str_hour_rest_monthly = str(int(hour_rest_monthly // 24)) + " 天 " + str(
+                int(hour_rest_monthly % 24) + 1) + " 小时"
         else:
-            str_hour_rest_monthly = str(int(hour_rest_monthly%24)+1) + " 小时"
+            str_hour_rest_monthly = str(int(hour_rest_monthly % 24) + 1) + " 小时"
         logger.info("仍需：" + str(sanity_usage) + " 理智  -  " + str_hour_rest + " / " + str_hour_rest_monthly + " (Prime)")
 
 
-
 def run_print_plan():
-
     print("\n\n\n\n\n\n\n\n\n\n\n\n")
 
     print_plan()
@@ -276,7 +287,7 @@ def run_print_plan():
 def clear_task_not_open():
     global list_not_open
     list_not_open = []
-    
+
 
 if __name__ == '__main__':
 
@@ -284,7 +295,7 @@ if __name__ == '__main__':
 
     run_print_plan()
     run_plan()
-    
+
     schedule.clear()
 
     schedule.every(1).hours.at(":10").do(run_ship)
@@ -346,7 +357,7 @@ if __name__ == '__main__':
     schedule.every(1).day.at("00:40").do(run_plan)
     schedule.every(1).day.at("01:40").do(run_plan)
     schedule.every(1).day.at("02:40").do(run_plan)
-    
+
     schedule.every(1).day.at("04:40").do(run_plan)
     schedule.every(1).day.at("05:40").do(run_plan)
     schedule.every(1).day.at("06:40").do(run_plan)
@@ -368,10 +379,9 @@ if __name__ == '__main__':
     schedule.every(1).day.at("22:40").do(run_plan)
     schedule.every(1).day.at("23:40").do(run_plan)
 
-
     while True:
         schedule.run_pending()
-        if schedule.idle_seconds()>0:
+        if schedule.idle_seconds() > 0:
             run_print_plan()
             timenow = datetime.datetime.now()
             nexttask_str = {
